ff9d01f2704592dbe9b192a773d7b8610a3d13da
jiuhuiyi365 <jiuhuiyi365@users.noreply.github.com>
2026-05-26 22:07:06 +0800

    友链数据写入配置文件，支持头像点击跳转

--- src/page_data/Link.ts
+++ src/page_data/Link.ts
@@ -2,5 +2,12 @@ export default {
   // API 接口请求优先，数据格式保持和 data 一致
   api: '',
   // api 为空则使用 data 静态数据
-  data: []
+  data: [
+    {
+      name: 'KelrinWu\'s Blog',
+      link: 'https://www.wqlblog.cn/',
+      avatar: 'https://www.wqlblog.cn/assets/images/avatar.png',
+      descr: '世界这么大，我想去看看.'
+    }
+  ]
 }
